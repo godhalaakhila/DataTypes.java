d282d7e41a8396c7325408cf7ed6daa2004319b3
godhalaakhila <akhilareddyg55@gmail.com>
2025-01-29 21:22:55 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Integer types
+        byte byteValue = 100;          // 1 byte
+        short shortValue = 3000;       // 2 bytes
+        int intValue = 100000;         // 4 bytes
+        long longValue = 10000000000L; // 8 bytes
+
+        // Floating-point types
+        float floatValue = 5.75f;      // 4 bytes
+        double doubleValue = 19.99;    // 8 bytes
+
+        // Character type
+        char charValue = 'A';          // 2 bytes
+
+        // Boolean type
+        boolean booleanValue = true;   // 1 bit
+
+        // Display the values of all the data types
+        System.out.println("Byte Value: " + byteValue);
+        System.out.println("Short Value: " + shortValue);
+        System.out.println("Int Value: " + intValue);
+        System.out.println("Long Value: " + longValue);
+        System.out.println("Float Value: " + floatValue);
+        System.out.println("Double Value: " + doubleValue);
+        System.out.println("Char Value: " + charValue);
+        System.out.println("Boolean Value: " + booleanValue);
+    }
+}
+ 
+    
+
